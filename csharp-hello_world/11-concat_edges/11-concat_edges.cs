144a243a424cd9697083dfe4c16ad697b4ec0301
David Adegboyega <davidadegboyega@Davids-MacBook-Air.local>
2025-01-11 21:09:53 +0100

    11 edges

--- csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -1,9 +1,11 @@
 using System;
+
 class Program
 {
     static void Main(string[] args)
     {
         string p1 = "object", p2 = "-oriented ", p3 = "programming", p4 = " in C#";
-        Console.WriteLine(p1 + p2 + p3 + p4);
+        string result = p1 + p2 + p3 + p4;
+        Console.WriteLine(result);
     }
 }
