490f22964ade8af4c8ab589327d6c372ed55026d
David Adegboyega <davidadegboyega@Davids-MacBook-Air.local>
2025-01-11 20:00:09 +0100

    9 print specifiers

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,12 +1,14 @@
 using System;
-class Program {
-    static void Main(string[] args) {
-        int caseNumber = 1; int caseNumber = 2; int caseNumber = 3; 
-  double percent = 0.00;  double amount = 0.00;
-        // Set values based on the caseNumber
-        if (caseNumber == 1) { percent = -44.98; amount = -9999.99; }
-        if (caseNumber == 2) { percent = 26.41; amount = 12345.68; }
-        if (caseNumber == 3) { percent = 0.00; amount = 0.00; }
-        Console.WriteLine($"Percent: {percent:0.00}%"); Console.WriteLine($"Currency: {amount:C}");
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double percent = 75.534;
+        double currency = 98765.4321;
+
+        Console.WriteLine($"Percent: {percent:F2}%");
+        Console.WriteLine($"Currency: {currency:C}");
     }
 }
+
