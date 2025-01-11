1ae4e082ab7093f1d3b4720be04f3893822822d0
David Adegboyega <davidadegboyega@Davids-MacBook-Air.local>
2025-01-11 20:19:53 +0100

    9 print specifier

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -1,14 +1,12 @@
-using System;
-using System.Globalization;
-class Program
+using System;                       
+using System.Globalization;          
+class Program                       
 {
-    static void Main(string[] args)
+    static void Main(string[] args)  
     {
-        double percent = 75.534;
-        double currency = 98765.4321;
+        double percent = 75.534, currency = 98765.4321;
         percent = Math.Round(percent, 2);
         Console.WriteLine($"Percent: {percent:F2}%");
-        CultureInfo culture = new CultureInfo("en-US");
-        Console.WriteLine($"Currency: {currency.ToString("C", culture)}");
-    }
+        Console.WriteLine($"Currency: {currency.ToString("C", new CultureInfo("en-US"))}");
+    }                                
 }
