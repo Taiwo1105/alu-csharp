85e33c4a78e8c8124da1e23229799007f6f3f53d
David Adegboyega <davidadegboyega@Davids-MacBook-Air.local>
2025-02-14 14:52:08 +0100

    enemy

---
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -0,0 +1,11 @@
+// 0-enemy.cs
+namespace Enemies
+{
+    /// <summary>
+    /// Class that defines a Zombie.
+    /// </summary>
+    public class Zombie
+    {
+        // Empty class for now
+    }
+}
