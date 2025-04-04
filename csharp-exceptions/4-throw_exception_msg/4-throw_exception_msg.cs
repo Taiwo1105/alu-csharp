acf4fb6cad91176a91570f6f9330dba2b81b601a
Taiwo1105 <t.adegboyeg@alustudent.com>
2025-04-04 13:50:08 +0200

    exception

---
+++ csharp-exceptions/4-throw_exception_msg/4-throw_exception_msg.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Except
+{
+    public static void ThrowMsg(string msg)
+    {
+        // Throwing an exception with a custom message
+        throw new Exception(msg);
+    }
+}
